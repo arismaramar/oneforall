0e12f82e1a9a9497d5cb75898d4f168961471f5e
oneforallofall <oneforall@wardao.eu.org>
2022-11-17 02:54:21 -0500

    ipv4

--- xray.sh
+++ xray.sh
@@ -253,12 +253,6 @@ getData() {
 
 IPV4=$(dig @1.1.1.1 +short  txt ch  whoami.cloudflare  |tr -d \")
 IPV6=$(dig +short @2606:4700:4700::1111 -6 ch txt whoami.cloudflare|tr -d \")
-resolve4="$(dig A  +short ${DOMAIN} @1.1.1.1)"
-resolve6="$(dig AAAA +short ${DOMAIN} @1.1.1.1)"
-res4=`echo -n ${resolve4} | grep $IPV4`
-res6=`echo -n ${resolve6} | grep $IPV6`
-res=`echo $res4$res6`
-IP=`echo $res4$res6`
 
     if [[ "$TLS" = "true" || "$XTLS" = "true" ]]; then
         echo ""
@@ -294,11 +288,20 @@ IP=`echo $res4$res6`
             resolve=`curl -sL http://ip-api.com/json/${DOMAIN}`
             res=`echo -n ${resolve} | grep ${IP}`
 
+IPV4=$(dig @1.1.1.1 +short  txt ch  whoami.cloudflare  |tr -d \")
+IPV6=$(dig +short @2606:4700:4700::1111 -6 ch txt whoami.cloudflare|tr -d \")
+resolve4="$(dig A  +short ${DOMAIN} @1.1.1.1)"
+resolve6="$(dig AAAA +short ${DOMAIN} @1.1.1.1)"
+res4=`echo -n ${resolve4} | grep $IPV4`
+res6=`echo -n ${resolve6} | grep $IPV6`
+res_judge=`echo $res4$res6`
+echo $res_judge
+IP=`echo $res4$res6`
 
-echo "${DOMAIN}  points to: $res"
+echo "${DOMAIN}  points to: ${res_judge}"
 
-            if [[ -z "${res}" ]]; then
-                colorEcho ${BLUE}  "${DOMAIN} 解析结果：${res}"
+            if [[ -z "${res_judge}" ]]; then
+                colorEcho ${BLUE}  "${DOMAIN} 解析结果：${res_judge}"
                 colorEcho ${RED}  " 域名未解析到当前服务器IP:$IPV4,$IPV6 !"
                 exit 1
             fi

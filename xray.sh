71d670a07099f9ed1cf33ecf0817b890c71de577
oneforallofall <oneforall@wardao.eu.org>
2022-11-17 02:33:09 -0500

    xray2

--- xray.sh
+++ xray.sh
@@ -254,7 +254,7 @@ getData() {
         echo ""
         echo " Xray一键脚本，运行之前请确认如下条件已经具备："
         colorEcho ${YELLOW} "  1. 一个伪装域名"
-        colorEcho ${YELLOW} "  2. 伪装域名DNS解析指向当前服务器ip（${IP}）"
+        colorEcho ${YELLOW} "  2. 伪装域名DNS解析指向当前服务器ip $IPV4,$IPV6"
         colorEcho ${BLUE} "  3. 如果/root目录下有 xray.pem 和 xray.key 证书密钥文件，无需理会条件2"
         echo " "
         read -p " 确认满足按y，按其他退出脚本：" answer

9386b049978082daa0a81c47a4b646432de7dde8
root <oneforall@wardao.eu.org>
2022-11-21 08:06:56 -0500

    xtls_http

--- oneforall.sh
+++ oneforall.sh
@@ -20,7 +20,8 @@ trojan='bash <(curl -fsSL  https://raw.githubusercontent.com/oneforallofall/onef
 v2ray='bash <(curl -fsSL  https://raw.githubusercontent.com/oneforallofall/oneforall/main/v2ray.sh)'
 tuic='bash <(curl -fsSL  https://raw.githubusercontent.com/oneforallofall/oneforall/main/tuic.sh)'
 curl -s --max-time 10 "https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Foneforallofall%2Fcount&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false" | tail -3 | head -n 1 | awk '{print $5,$7}' >/dev/null 2>&1
-xray='bash <(curl -fsSL  https://raw.githubusercontent.com/oneforallofall/oneforall/main/xray.sh)'
+xray='bash <(curl -fsSL  https://raw.githubusercontent.com/oneforallofall/oneforall/main/xray.sh )'
+xtls_http='bash <(curl -fsSL  https://raw.githubusercontent.com/oneforallofall/oneforall/main/xtls_http.sh  '
 #xray='bash <(curl -sL https://raw.githubusercontent.com/daveleung/hijkpw-scripts-mod/main/xray_mod1.sh)'
 hysteria='bash <(curl -fsSL https://git.io/hysteria.sh)'
 IO='wget -qO- git.io/superbench.sh | bash'
@@ -41,6 +42,7 @@ ${Red_font_prefix}6${Font_color_suffix} IO硬盘和网络速度测试
 ${Red_font_prefix}7${Font_color_suffix} 回程路由
 ${Red_font_prefix}8${Font_color_suffix} 回程路由(详细)
 ${Red_font_prefix}9${Font_color_suffix} 流媒体解锁测试
+${Red_font_prefix}10${Font_color_suffix} vless xtls 下载神器跑满宽带(自己找手机网址，有可能免流 )
 
 \r\n
 ")" choose
@@ -54,6 +56,7 @@ ${Red_font_prefix}9${Font_color_suffix} 流媒体解锁测试
 		7) eval $back_route ;;
 		8) eval $back_route_de ;;
 		9) eval $media ;;
+		10) eval $xtls_http;;
 	 
 		*) echo "wrong input" ;;
 	esac
